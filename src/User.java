d9fd8d4512445656b6139ee16d9aada1cec6abf9
Fatemeh Roshan <famazlom@gmail.com>
2022-06-28 01:31:21 +0430

    add user class and some methods that we need

--- src/User.java
+++ src/User.java
@@ -1,18 +1,44 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.Socket;
-public class User {
-    Socket socket;
-    DataOutputStream dos;
-    DataInputStream dis;
-    public User(String ip, int port) {
-        try {
-            socket = new Socket(ip, port);
-            dis = new DataInputStream(socket.getInputStream());
-            dos = new DataOutputStream(socket.getOutputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
+import java.util.HashMap;
+public class User{
+  private final HashMap<String,String> data;
+
+    public User(HashMap<String, String> data) {
+        this.data = data;
+    }
+    public HashMap<String, String> getData() {
+        return data;
+    }
+
+    String doWeHaveThisId(){
+        String users = DataBase.getSingleTone().getController("UsersInformation").readFile();
+        String[] split = users.split("\n");
+        for (String str : split) {
+            if (str.startsWith(data.get("userId"))) {
+                return str;
+            }
         }
+        return "invalid";
+    }
+
+    String signUp() {
+        DataBase.getSingleTone().getController("UsersInformation").writeFile(data.get("userId")
+                + ": {, " + data.get("password") + ", null, null, null, null, null, }\n");
+        DataBase.getSingleTone().getController("UserCommunities").writeFile(data.get("userId") + ": {, }\n");
+        DataBase.userCounter++;
+        return "valid";
+    }
+
+    String signIn() {
+        String userId = doWeHaveThisId();
+        if (userId.equals("invalid")) {
+            return "invalid";
+        } else if (!userId.split(", ")[1].equals(data.get("password"))) {
+            return "invalid-match";
+        }
+        return "valid";
+    }
+
+    String getAccount() {
+        return DataBase.getSingleTone().getController("UsersInformation").getRow(data.get("userId"));
     }
 }
