65ea66510f408e71a752883ed1bb0ae676682a57
Fatemeh Roshan <famazlom@gmail.com>
2022-06-30 11:00:37 +0430

    add editEmail and editId

--- src/User.java
+++ src/User.java
@@ -1,14 +1,16 @@
 import java.util.HashMap;
-public class User{
-  private final HashMap<String,String> data;
+public class User {
+    private final HashMap<String, String> data;
+
     public User(HashMap<String, String> data) {
         this.data = data;
     }
+
     public HashMap<String, String> getData() {
         return data;
     }
 
-    String doWeHaveThisId(){
+    String doWeHaveThisId() {
         String users = DataBase.getSingleTone().getController("UsersInformation").readFile();
         String[] split = users.split("\n");
         for (String str : split) {
@@ -34,15 +36,15 @@ public class User{
         String userId = doWeHaveThisId();
         String userEmail = doWeHaveThisEmail();
 
-        if(!(userEmail.equals("_newEmail"))){
+        if (!(userEmail.equals("_newEmail"))) {
             return "_invalid";
         }
-        if (!(userId.equals("_newId"))){
+        if (!(userId.equals("_newId"))) {
             return "_invalid";
         }
         DataBase.getSingleTone().getController("UsersInformation").writeFile(data.get("userId")
-                + ", " + data.get("userEmail")+", "+data.get("password") + ", \n");
-        DataBase.getSingleTone().getController("UsersFollowingCommunities").writeFile(data.get("userId") + ", " + data.get("userEmail")+ ", \n");
+                + ", " + data.get("userEmail") + ", " + data.get("password") + ", \n");
+        DataBase.getSingleTone().getController("UsersFollowingCommunities").writeFile(data.get("userId") + ", " + data.get("userEmail") + ", \n");
         DataBase.userCounter++;
         return "_valid";
     }
@@ -61,5 +63,52 @@ public class User{
         return DataBase.getSingleTone().getController("UsersInformation").getRow(data.get("userId"));
     }
 
+    String editId() {
+        if (!(doWeHaveThisId().equals("_newId"))) {
+            return "_invalid";
+        }
+        String users = DataBase.getSingleTone().getController("UsersInformation").readFile();
+        String[] split = users.split("\n");
+        StringBuilder ans = new StringBuilder();
 
+        for (String str : split) {
+            if (str.startsWith(data.get("userId"))) {
+                String[] update = str.split(", ");
+                update[0] = data.get("newId");
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < update.length; i++) {
+                    stringBuilder.append(update[0]).append(", ");
+                }
+                str = stringBuilder.toString();
+            }
+            ans.append(str).append("\n");
+        }
+        DataBase.getSingleTone().getController("UsersInformation").writeFile(ans.toString(), true);
+        return "_valid";
+    }
+
+    String editEmail() {
+        if (!(doWeHaveThisEmail().equals("_newEmail"))) {
+            return "_invalid";
+        }
+        String users = DataBase.getSingleTone().getController("UsersInformation").readFile();
+        String[] split = users.split("\n");
+        StringBuilder ans = new StringBuilder();
+
+        for (String str : split) {
+            if (str.split(", ")[1].equals(data.get("userEmail"))) {
+                String[] update = str.split(", ");
+                update[1] = data.get("newEmail");
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < update.length; i++) {
+                    stringBuilder.append(update[0]).append(", ");
+                }
+                str = stringBuilder.toString();
+            }
+            ans.append(str).append("\n");
+        }
+        DataBase.getSingleTone().getController("UsersInformation").writeFile(ans.toString(), true);
+        return "_valid";
+    }
 }
+
